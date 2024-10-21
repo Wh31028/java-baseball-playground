3c2aad354dfa35110c92f6e1718c33d9909e9308
Wh31028 <wh31028@naver.com>
2024-10-21 21:19:38 +0900

    feat(스트라이크 볼 판정 구현)

--- src/main/java/baseball/domain/Judgment.java
+++ src/main/java/baseball/domain/Judgment.java
@@ -1,4 +1,16 @@
 package baseball.domain;
 
+import java.util.ArrayList;
+
 public class Judgment {
+    public int judge(ArrayList<Integer> answer, ArrayList<Integer> input) {
+        int ball = 0;
+         MatchNum matchNum = new MatchNum();
+         MatchStrike matchStrike = new MatchStrike();
+         int strike = matchStrike.matchStrike(answer, input);
+         ball = matchNum.match(answer, input)- strike;
+        if(ball>0) System.out.print(ball+"볼 ");
+        if(strike>0) System.out.print(strike+"스트라이크");
+        return strike;
+    }
 }
